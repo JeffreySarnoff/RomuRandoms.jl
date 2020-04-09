0c2779b5b1cddfe4d883fffe1d5d1a1a13c9d590
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-04-09 09:20:32 -0400

    Create romu_states.jl

---
+++ src/romu_states.jl
@@ -0,0 +1,136 @@
+struct RomuQuadState
+    wstate::UInt64
+    xstate::UInt64
+    ystate::UInt64
+    zstate::UInt64
+end
+
+struct RomuTrioState
+    xstate::UInt64
+    ystate::UInt64
+    zstate::UInt64
+end
+
+struct RomuDuoState
+    xstate::UInt64
+    ystate::UInt64
+end
+
+struct RomuDuoJrState
+    xstate::UInt64
+    ystate::UInt64
+end
+
+struct RomuQuad32State
+    wstate::UInt32
+    xstate::UInt32
+    ystate::UInt32
+    zstate::UInt32
+end
+
+struct RomuTrio32State
+    xstate::UInt32
+    ystate::UInt32
+    zstate::UInt32
+end
+
+struct RomuMono32State
+    xstate::UInt32
+end
+
+
+function setstate(::Type{RomuQuadState}, w::T=0, x::T=0, y::T=0, z::T=0) where {T<:UInt64}
+    validate(RomuQuadState, w, x, y, z)
+    return RomuQuadState(w, x, y, z)
+end
+
+function setstate(::Type{RomuTrioState}, x::T=0, y::T=0, z::T=0) where {T<:UInt64}
+    validate(RomuTrioState, x, y, z)
+    return RomuTrioState(w, x, y, z)
+end
+
+function setstate(::Type{RomuDuoState}, x::T=0, y::T=0) where {T<:UInt64}
+    validate(RomuDuoState, x, y)
+    return RomuDuoState(x, y)
+end
+
+function setstate(::Type{RomuDuoJrState}, x::T=0, y::T=0) where {T<:UInt64}
+    validate(RomuDuoJrState, x, y)
+    return RomuDuoJrState(x, y)
+enduuint32rangeange
+
+function setstate(::Type{RomuQuad32State}, w::T=0, x::T=0, y::T=0, z::T=0) where {T<:UInt32}
+    validate(RomuQuad32State, w, x, y, z)
+    return RomuQuad32State(w, x, y, z)
+end
+
+function setstate(::Type{RomuTrio32State}, x::T=0, y::T=0, z::T=0) where {T<:UInt32}
+    validate(RomuTrio32State, x, y, z)
+    return RomuTrio32State(w, x, y, z)
+end
+
+function setstate(::Type{RomuMono32State}, x::T=0) where {T<:UInt32}
+    validate(RomuMono32State, x)
+    return RomuMono32State(x)
+end
+
+const uint64range = UnitRange(one(UInt64), typemax(UInt64))
+const uint32range = UnitRange(one(UInt32), typemax(UInt32))
+
+setstate(::Type{RomuQuadState}) = setstate(RomuQuadState, rand(uint64range, 4)...)
+setstate(::Type{RomuTrioState}) = setstate(RomuTrioState, rand(uint64range, 3)...)
+setstate(::Type{RomuDuoState}) = setstate(RomuDuoState, rand(uint64range, 2)...)
+setstate(::Type{RomuDuoJrState}) = setstate(RomuDuoJrState, rand(uint64range, 2)...)
+setstate(::Type{RomuQuad32State}) = setstate(RomuQuad32State, rand(uint32range, 4)...)
+setstate(::Type{RomuTrio32State}) = setstate(RomuTrio32State, rand(uint32range, 3)...)
+setstate(::Type{RomuMono32State}) = setstate(RomuMono32State, rand(uint32range, 2)...)
+
+
+validate(::Type{RomuQuadState}, w::T=0, x::T=0, y::T=0, z::T=0) where {T<:UInt64} =
+   if iszero(w) || iszero(x) || iszero(y) || iszero(z)
+       throw(RomuException("All four states must be initialized to nonzero values."))
+   else
+       nothing
+   end
+
+validate(::Type{RomuTrioState}, x::T=0, y::T=0, z::T=0) where {T<:UInt64} =
+   if iszero(x) || iszero(y) || iszero(z)
+       throw(RomuException("All three states must be initialized to nonzero values."))
+   else
+       nothing
+   end
+
+validate(::Type{RomuDuoState}, x::T=0, y::T=0) where {T<:UInt64} =
+   if iszero(x) || iszero(y)
+       throw(RomuException("Both states must be initialized to nonzero values."))
+   else
+       nothing
+   end
+
+validate(::Type{RomuDuoJrState}, x::T=0, y::T=0) where {T<:UInt64} =
+   if iszero(x) || iszero(y)
+       throw(RomuException("Both states must be initialized to nonzero values."))
+   else
+       nothing
+   end
+
+validate(::Type{RomuQuad32State}, w::T=0, x::T=0, y::T=0, z::T=0) where {T<:UInt32} =
+   if iszero(w) || iszero(x) || iszero(y) || iszero(z)
+       throw(RomuException("All four states must be initialized to nonzero values."))
+   else
+       nothing
+   end
+
+validate(::Type{RomuTrio32State}, x::T=0, y::T=0, z::T=0) where {T<:UInt32} =
+   if iszero(x) || iszero(y) || iszero(z)
+       throw(RomuException("All three states must be initialized to nonzero values."))
+   else
+       nothing
+   end
+
+validate(::Type{RomuMono32State}, x::T=0) where {T<:UInt32} =
+   if iszero(x)
+       throw(RomuException("The state must be initialized to a nonzero value."))
+   else
+       nothing
+   end
