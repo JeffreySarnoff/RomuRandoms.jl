239f460a8809c72e7c0651fec6e582e8ca2d51f4
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-04-09 08:12:05 -0400

    Update RomuRandoms.jl

--- src/RomuRandoms.jl
+++ src/RomuRandoms.jl
@@ -1,7 +1,11 @@
 module RomuRandoms
 
-export RomuQuad, RomuTrio, RomuDuo, RomuDuoJr, RomuQuad32, RomuTrio32, RomuMono32
+export romu, romuquad, romutrio, romuduo, romuduojr, romuquad32, romutrio32, romumono32
 
 include("primitives.jl") # rotate bits (ror, rol)
+include("romu64s.jl")    # romuquad, romutrio, romuduo, romuduojr
+include("romu32s.jl")    # romuquad32, romutrio32, romumono32
+
+const romu = romutrio
 
 end #  RomuRandoms
