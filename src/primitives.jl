aeea3750b184f64208dea043d88505358aef3a7c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-04-09 10:11:06 -0400

    Update primitives.jl

--- src/primitives.jl
+++ src/primitives.jl
@@ -10,6 +10,11 @@ rotate x to the right (msbs toward lsb) by k bits (lsbs wrap to msbs)
 rotate x to the left (lsbs toward msb) by k bits (msbs wrap to lsbs)
 """ rol
 
+ror(x::UInt32, k::Int) = (x >>> (0x1f & k)) | (x <<  (0x1f & -k))
+rol(x::UInt32, k::Int) = (x <<  (0x1f & k)) | (x >>> (0x1f & -k))
+ror(x::UInt64, k::Int) = (x >>> (0x3f & k)) | (x <<  (0x3f & -k))
+rol(x::UInt64, k::Int) = (x <<  (0x3f & k)) | (x >>> (0x3f & -k))
+
 ror(x::Int32, k::Int) = (x >>> (0x1f & k)) | (x <<  (0x1f & -k))
 rol(x::Int32, k::Int) = (x <<  (0x1f & k)) | (x >>> (0x1f & -k))
 ror(x::Int64, k::Int) = (x >>> (0x3f & k)) | (x <<  (0x3f & -k))
