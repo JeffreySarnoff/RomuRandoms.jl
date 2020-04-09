afe08d54856dc5844eaf617aa79bd21c67590da7
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-04-09 11:32:44 -0400

    Update splitmix.c

--- docs/splitmix.c
+++ docs/splitmix.c
@@ -39,3 +39,29 @@ z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
 z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
 return z ^ (z >> 31);
 }
+
+
+// Copyright 2020 Mark A. Overton
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include <stdint.h>
+static uint32_t sState; // State can be seeded with any value, including 0.
+
+uint32_t splitMix32 () {
+	sState = 1664525u * (sState + 314159265u); // Multiplier is from Knuth.
+	uint32_t z = sState;
+	z = (z ^ (z >> 15)) * 0x5ce4e5b9u;
+	z = (z ^ (z >> 13)) * 0x1331c1ebu;
+	return z ^ (z >> 15);
+}
+
