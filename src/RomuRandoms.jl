abadd51faf6e533dad5d2a5fa0c8d750c6f2b264
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-04-09 08:55:33 -0400

    Update RomuRandoms.jl

--- src/RomuRandoms.jl
+++ src/RomuRandoms.jl
@@ -2,6 +2,10 @@ module RomuRandoms
 
 export romu, romuquad, romutrio, romuduo, romuduojr, romuquad32, romutrio32, romumono32
 
+struct RomuException <: exception
+    msg::AbstractString
+end
+
 include("primitives.jl") # rotate bits (ror, rol)
 include("romu64s.jl")    # romuquad, romutrio, romuduo, romuduojr
 include("romu32s.jl")    # romuquad32, romutrio32, romumono32
