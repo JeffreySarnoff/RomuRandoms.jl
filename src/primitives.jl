6eb04c03ea7079c4707d5ab26b2406f79c98cd26
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-04-09 08:07:55 -0400

    Create primitives.jl

---
+++ src/primitives.jl
@@ -0,0 +1,16 @@
+"""
+    ror(x, k)
+    
+rotate x to the right (msbs toward lsb) by k bits (lsbs wrap to msbs)
+""" ror
+
+"""
+    rol(x, k)
+    
+rotate x to the left (lsbs toward msb) by k bits (msbs wrap to lsbs)
+""" rol
+
+ror(x::Int32, k::Int) = (x >>> (0x1f & k)) | (x <<  (0x1f & -k))
+rol(x::Int32, k::Int) = (x <<  (0x1f & k)) | (x >>> (0x1f & -k))
+ror(x::Int64, k::Int) = (x >>> (0x3f & k)) | (x <<  (0x3f & -k))
+rol(x::Int64, k::Int) = (x <<  (0x3f & k)) | (x >>> (0x3f & -k))
