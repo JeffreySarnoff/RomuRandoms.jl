4389aec86ecafdbe2594ec23d6b1d86fcfdb090f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-04-09 09:21:20 -0400

    Update RomuRandoms.jl

--- src/RomuRandoms.jl
+++ src/RomuRandoms.jl
@@ -6,9 +6,10 @@ struct RomuException <: exception
     msg::AbstractString
 end
 
-include("primitives.jl") # rotate bits (ror, rol)
-include("romu64s.jl")    # romuquad, romutrio, romuduo, romuduojr
-include("romu32s.jl")    # romuquad32, romutrio32, romumono32
+include("primitives.jl")  # rotate bits (ror, rol)
+include("romu_states.jl") # setstate, state structs 
+include("romu64s.jl")     # romuquad, romutrio, romuduo, romuduojr
+include("romu32s.jl")     # romuquad32, romutrio32, romumono32
 
 const romu = romutrio
 
